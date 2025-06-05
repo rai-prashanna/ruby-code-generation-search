bce62029f760f8fb193843e341341babfdfc3870
PRASHANNA RAI <prai931024@gmail.com>
2025-06-05 15:52:12 +0200

    batch uploading of vector embedding into MilVusDb

--- CodeUtility.py
+++ CodeUtility.py
@@ -6,6 +6,8 @@ import torch
 from fastembed import TextEmbedding
 from tqdm import tqdm
 
+import MilvusDBUtility
+
 torch.set_num_threads(1)
 
 def extract_subclass_and_parent_class(signature: str) -> str:
@@ -52,7 +54,6 @@ def generate_prompt_from_code_metadata(chunk: Dict[str, Any]) -> str:
     code_snippet_text=f" has code snippets {chunk['context']['snippet']} "
     text_rep = f"{chunk['code_type']} {name} {base_class_text} {docstring_text} defined as {signature} {context} {code_snippet_text}"
     tokens = re.split(r"\W", text_rep)
-    print("Done with generating text representations...")
     return " ".join(filter(None, tokens))
 
 def transform_data_to_vector_embedding(text_representations:list,model) -> list:
@@ -67,10 +68,23 @@ def transform_data_to_vector_embedding(text_representations:list,model) -> list:
 
 code_meta_datas,code_snippets=load_json_into_memory("raw.jsonl")
 natural_language_representations = list(map(generate_prompt_from_code_metadata, code_meta_datas))
+print("Done with generating text representations...")
+
 print("Generating Natural language embedding ...")
 natural_language_model = TextEmbedding("sentence-transformers/all-MiniLM-L6-v2", threads=0)
 natural_language_vector_embedding=transform_data_to_vector_embedding(natural_language_representations, natural_language_model)
 print("Generating Code embedding ...")
 code_model = TextEmbedding("jinaai/jina-embeddings-v2-base-code", threads=0)
 code_snippets_vector_embedding=transform_data_to_vector_embedding(code_snippets, code_model)
+print("Create collection or load collection ...")
+collection_name="temp"
+my_collection=MilvusDBUtility.create_collection(collection_name)
+MilvusDBUtility.batch_upload(my_collection, natural_language_vector_embedding, code_snippets_vector_embedding, code_meta_datas, 5)
+
 print("Done...")
+
+
+
+
+
+
--- MilvusDBUtility.py
+++ MilvusDBUtility.py
@@ -1,9 +1,31 @@
+import json
 from uuid import uuid4
-from pymilvus import Collection
 from tqdm import tqdm
 from pymilvus import (connections,utility, FieldSchema, CollectionSchema, DataType, Collection)
 from pymilvus import Index
 
+def transform_data_to_vector_embedding(text_representations:list,model) -> list:
+    vector_embeddings = list(tqdm(
+        model.embed(text_representations, batch_size=5),
+        total=len(text_representations),
+        desc="Text Embedding"
+    ))
+    print("Done with embedding process...")
+    return vector_embeddings
+
+
+def transform_single_data_to_vector_embedding(text: str, model) -> list:
+    print("Generating embedding for single text...")
+    vector_embedding = model.embed([text]) [0]# Assuming model.embed returns a list of embeddings
+    print("Done with embedding process.")
+    return vector_embedding
+
+
+# def transform_data_to_vector_embedding(text_representation,model) -> list:
+#     vector_embeddings =model.embed(text_representation)
+#
+#     print("Done with embedding process...")
+#     return vector_embeddings
 
 def token_length(self, text):
     tokens = self.tokenizer.encode(text)
@@ -11,7 +33,9 @@ def token_length(self, text):
 
 def create_connection():
     # Connect to Milvus
+    print("Create connection ...")
     connections.connect("default", host="localhost", port="19530")
+    print("connection created...")
 
 def create_collection_on_empty_db( collection_name = "milvus_llm_example"):
     # create collection if collection dose not exists
@@ -22,21 +46,26 @@ def create_collection_on_empty_db( collection_name = "milvus_llm_example"):
         print(f"Collection named {collection_name} created!")
 
 def create_collection( collection_name) -> Collection:
+    create_connection()
     dim=768
     dim_text = 384  # e.g. "all-MiniLM-L6-v2" outputs 384‐dim vectors
     dim_code = 768  # e.g. "jinaai/jina-embeddings-v2-base-code" outputs 768‐dim
-
-    fields = [
-        FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+    # FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+    fields = [ FieldSchema( name="ids", dtype=DataType.VARCHAR, is_primary=True,auto_id=False, max_length=36),
         FieldSchema(name="text_embedding", dtype=DataType.FLOAT_VECTOR, dim=dim_text),
         FieldSchema(name="code_embedding", dtype=DataType.FLOAT_VECTOR, dim=dim_code),
-        FieldSchema(name="code_metadata", dtype=DataType.VARCHAR),
+        FieldSchema(name="code_metadata", dtype=DataType.VARCHAR,max_length=65534),
     ]
-    schema = CollectionSchema(fields, f" stores both text & code embeddings + payload")
+    schema = CollectionSchema(fields, f" stores both text & code embeddings with code-metadata")
 
     print(f"Create collection {collection_name}")
-    # Connect to collection and show size
-    collection = Collection(collection_name)
+    if not utility.has_collection(collection_name):
+        collection = Collection(name=collection_name, schema=schema)
+    else:
+        utility.drop_collection(collection_name)
+        collection = Collection(name=collection_name, schema=schema)
+        # collection = Collection(name=collection_name)
+
     print(f"Done with creation of {collection_name} Collection")
     index_params_text = {
         "index_type": "IVF_FLAT",  # or HNSW, etc.
@@ -55,38 +84,63 @@ def create_collection( collection_name) -> Collection:
     print(collection.num_entities)
     return  collection
 
-# def insert(natural_language_embedding, code_embedding, code_metadata):
 
 
-def insert_data_in_batch(collection,natural_language_embeddings, code_embeddings, code_metadata_list):
-    insert_data = [
+
+def batch_upload(my_collection, natural_language_embeddings, code_embeddings, code_metadata_lists, batch_size):
+    batch_nl_vectors = []
+    batch_code_vectors = []
+    batch_code_metadata_lists = []
+    batch_ids =[]
+    # batch_ids= [str(uuid4()) for _ in range(len(code_metadata_lists))]
+    for index,code_metadata in tqdm(enumerate(code_metadata_lists)):
+        batch_nl_vectors.append(natural_language_embeddings[index])
+        batch_code_vectors.append(code_embeddings[index])
+        batch_code_metadata_lists.append(json.dumps(code_metadata_lists[index]))
+        batch_ids.append(str(uuid4()))
+        if len(batch_nl_vectors) >= batch_size:
+            insert_data_in_batch(my_collection, batch_ids, batch_nl_vectors, batch_code_vectors, batch_code_metadata_lists)
+            batch_code_vectors=[]
+            batch_nl_vectors=[]
+            batch_code_metadata_lists=[]
+            batch_ids=[]
+    if len(batch_nl_vectors) > 0:
+        insert_data_in_batch(my_collection, batch_ids, batch_nl_vectors, batch_code_vectors, batch_code_metadata_lists)
+    my_collection.flush()
+    #collection.release()
+
+
+def insert_data_in_batch(collection,ids,natural_language_embeddings, code_embeddings, code_metadata_list):
+    insert_data = [ids,
         natural_language_embeddings,  # "text_embedding" FLOAT_VECTOR
         code_embeddings,  # "code_embedding" FLOAT_VECTOR
         code_metadata_list,  # "payload" VARCHAR
     ]
     collection.insert(insert_data)
 
-def batch_upload(collection,natural_language_embeddings, code_embeddings, code_metadata_lists,batch_size):
+
+def batch_encoding_and_upload(my_connection,natural_language_model, code_model, code_metadata_lists,natural_language_representations,code_snippets,batch_size):
     batch_nl_vectors = []
     batch_code_vectors = []
     batch_code_metadata_lists = []
-    for natural_language, code_snippet, code_metadata in tqdm(zip(natural_language_embeddings, code_embeddings, code_metadata_lists)):
-        print("natural")
-        batch_nl_vectors.extend(natural_language)
-        batch_code_vectors.extend(code_snippet)
-        batch_code_metadata_lists.extend(code_metadata)
+    batch_ids =[]
+    # batch_ids= [str(uuid4()) for _ in range(len(code_metadata_lists))]
+    for index,code_metadata in tqdm(enumerate(code_metadata_lists)):
+        natural_language_embedding=transform_single_data_to_vector_embedding(natural_language_representations[index], natural_language_model)
+        code_snippets_vector_embedding = transform_single_data_to_vector_embedding(code_snippets[index],code_model)
+        batch_nl_vectors.append(natural_language_embedding)
+        batch_code_vectors.append(code_snippets_vector_embedding)
+        batch_code_metadata_lists.append(json.dumps(code_metadata))
+        batch_ids.append(str(uuid4()))
         if len(batch_nl_vectors) >= batch_size:
-            insert_data_in_batch(collection,batch_nl_vectors, batch_code_vectors, batch_code_metadata_lists)
+            insert_data_in_batch(my_connection,batch_ids,batch_nl_vectors, batch_code_vectors, batch_code_metadata_lists)
             batch_code_vectors=[]
             batch_nl_vectors=[]
             batch_code_metadata_lists=[]
+            batch_ids=[]
     if len(batch_nl_vectors) > 0:
-        insert_data_in_batch(collection, batch_nl_vectors, batch_code_vectors, batch_code_metadata_lists)
-    collection.flush()
+        insert_data_in_batch(my_connection, batch_ids,batch_nl_vectors, batch_code_vectors, batch_code_metadata_lists)
+    insert_data_in_batch(my_connection, batch_ids, batch_nl_vectors, batch_code_vectors, batch_code_metadata_lists)
+    my_connection.flush()
 
-
-def upload_batch(self, texts, metadatas):
-    ids = [str(uuid4()) for _ in range(len(texts))]
-    embeddings = self.embedder.encode(texts)
-    self.collection.insert([ids, embeddings, metadatas])
 
